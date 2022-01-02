57a5b35a4676be05d00009b513977d5ddf948190
Janek <62259501+janskwr@users.noreply.github.com>
2022-01-02 15:09:05 +0100

    Update 00001_two_sum.py

--- 00001_two_sum.py
+++ 00001_two_sum.py
@@ -1,6 +1,7 @@
 """
-SOLUTION 1
+SOLUTION 1 - HASH TABLE
 Time Complexity: O(n)
+Space Complexity: O(n)
 """
 
 class Solution:
@@ -13,3 +14,17 @@ class Solution:
                 return [i, hashmap[result]]
             hashmap[nums[i]] = i
             
+
+"""
+SOLUTION 2 - BRUTE FORCE
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+ 
