7f25216076fc0aed287b7c8f9cb2acebb48c1dd4
Janek <62259501+janskwr@users.noreply.github.com>
2022-07-05 18:12:31 +0200

    Add files via upload

--- 00005_longest_palindromic_substring.py
+++ 00005_longest_palindromic_substring.py
@@ -1,24 +1,63 @@
 """
-SOLUTION 1
+SOLUTION 1 - LONGEST COMMON SUBSTRING IMPROVED
 Time Complexity: O(n^2)
+Space Complexity: O(n)
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+    	n = len(s)
+    	max_len = 0
+    	longest = ""
+    	rev = s[::-1]
+    	dp = [0 for i in range(n)]
+    	for i in range(n):
+    	    	
+
+
+
+"""
+SOLUTION 2 - LONGEST COMMON SUBSTRING
+Time Complexity: O(n^2)
+Space Complexity: O(n^2)
 """
 
 class Solution:
-    def longestPalindrome(self, s):
-        res = ""
-        for i in range(len(s)):
-            tmp = self.helper(s, i, i)
-            if len(tmp) > len(res):
-                res = tmp
-            tmp = self.helper(s, i, i + 1)
-            if len(tmp) > len(res):
-                res = tmp
-        return res
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        rev = s[::-1]
+        dp = [[0 for i in range(n)] for j in range(n)]
+        longest = ""
+        max_len = 0
+        for i in range(n):
+            for j in range(n):
+                if s[i] == rev[j]:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > max_len:
+                    if i - dp[i][j] + 1 == n - j - 1:
+                        max_len = dp[i][j]
+                        longest = s[i - max_len + 1:i + 1]
+        return longest
 
+    
 
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return s[l + 1: r]
-        
+"""
+SOLUTION 3 - BRUTE FORCE
+Time Complexity: O(n^3)
+Space Complexity: O(1)
+"""
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        longest = ""
+        for i in range(n):
+            for j in range(i, n):
+                temp = s[i:j + 1]
+                if temp == temp[::-1]:
+                    if len(temp) > len(longest):
+                        longest = temp
+        return longest
+
